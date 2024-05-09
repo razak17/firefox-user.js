29539c78b8449380d60a1a75b2e0c8188d9a1659
Razak Mo <moabdulrazak17@gmail.com>
2024-05-09 19:10:53 +0000

    feat: refator with new improvements

--- setup.sh
+++ setup.sh
@@ -11,96 +11,157 @@ mkdir -p "$HOME/.dots"
 mkdir -p "$TMP"
 
 install_essentials() {
-  if [ ! -e "$TMP/user.js" ]; then
-    if curl -s -L "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" -o "${TMP}/user.js"; then
-      echo "User.js file downloaded"
-    fi
-  fi
+	if [ ! -e "$TMP/user.js" ]; then
+		if curl -s -L "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" -o "${TMP}/user.js"; then
+			echo "User.js file downloaded"
+		fi
+	fi
 
-  if [ ! -e "$TMP/updater.sh" ]; then
-    if curl -s -L "https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh" -o "${TMP}/updater.sh"; then
-      echo "Update script downloaded"
-    fi
-  fi
+	if [ ! -e "$TMP/updater.sh" ]; then
+		if curl -s -L "https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh" -o "${TMP}/updater.sh"; then
+			echo "Update script downloaded"
+		fi
+	fi
 
-  if [ ! -e "$TMP/prefsCleaner.sh" ]; then
-    if curl -s -L "https://raw.githubusercontent.com/arkenfox/user.js/master/prefsCleaner.sh" -o "${TMP}/prefsCleaner.sh"; then
-      echo "prefsCleaner script downloaded"
-    fi
-  fi
+	if [ ! -e "$TMP/prefsCleaner.sh" ]; then
+		if curl -s -L "https://raw.githubusercontent.com/arkenfox/user.js/master/prefsCleaner.sh" -o "${TMP}/prefsCleaner.sh"; then
+			echo "prefsCleaner script downloaded"
+		fi
+	fi
 
-  if [ ! -e "$TMP/updater.sh" ] || [ ! -e "$TMP/prefsCleaner.sh" ] || [ ! -e "$TMP/user.js" ]; then
-    echo "Error downloading scripts"
-    exit 1
-  fi
+	if [ ! -e "$TMP/updater.sh" ] || [ ! -e "$TMP/prefsCleaner.sh" ] || [ ! -e "$TMP/user.js" ]; then
+		echo "Error downloading scripts"
+		exit 1
+	fi
 }
 
 clone_config() {
-  if [ ! -d "$HOME/.dots/firefox-user.js" ]; then
-    echo "Cloning firefox-user.js"
-    git clone https://github.com/razak17/firefox-user.js "$HOME"/.dots/firefox-user.js
-  else
-    echo "Remove '$HOME/.dots/firefox-user.js' and run again"
-  fi
+	if [ ! -d "$HOME/.dots/firefox-user.js" ]; then
+		echo "Cloning firefox-user.js"
+		git clone https://github.com/razak17/firefox-user.js "$HOME"/.dots/firefox-user.js
+	else
+		echo "Remove '$HOME/.dots/firefox-user.js' and run again"
+	fi
 }
 
-config_branch() {
-  profile="$1"
-  mkdir -p "$FIREFOX_HOME/$profile"
+setup() {
+	profile="$1"
+	config="$2"
 
-  pushd "$HOME/.dots/firefox-user.js" || exit
-  if [ -d "$FIREFOX_HOME/$profile/chrome" ]; then
-    mv "$FIREFOX_HOME/$profile/chrome" "$FIREFOX_HOME/$profile/chrome-$(date +%F_%H%M%S_%N)"
-  fi
-  mkdir -p "$FIREFOX_HOME/$profile/chrome"
-  if [ "$profile" == "rec" ]; then
-    cp -R ./chrome/ui ./chrome/content ./chrome/*-"$profile"/* "$FIREFOX_HOME/$profile/chrome"
-  else
-    cp -R ./chrome/ui ./chrome/content ./chrome/*-coding/* "$FIREFOX_HOME/$profile/chrome"
-  fi
+	mkdir -p "$FIREFOX_HOME/$profile"
 
-  if [ -d "$FIREFOX_HOME/$profile/user.js-overrides" ]; then
-    mv "$FIREFOX_HOME/$profile/user.js-overrides" "$FIREFOX_HOME/$profile/user.js-overrides-$(date +%F_%H%M%S_%N)"
-  fi
-  mkdir -p "$FIREFOX_HOME/$profile/user.js-overrides"
-  if [ "$profile" == "coding" ] || [ "$profile" == "rec" ] || [ "$profile" == "dev" ] || [ "$profile" == "main" ] || [ "$profile" == "rgt" ]; then
-    cp -R ./user.js-overrides/_base.js ./user.js-overrides/*-"$profile"/* "$FIREFOX_HOME/$profile/user.js-overrides"
-  elif [ "$profile" == "social" ]; then
-    cp -R ./user.js-overrides/_base.js ./user.js-overrides/*-dev/* "$FIREFOX_HOME/$profile/user.js-overrides"
-  else
-    cp -R ./user.js-overrides/_base.js ./user.js-overrides/*-coding/* "$FIREFOX_HOME/$profile/user.js-overrides"
-  fi
+	pushd "$HOME/.dots/firefox-user.js" || exit
+	if [ -d "$FIREFOX_HOME/$profile/chrome" ]; then
+		mv "$FIREFOX_HOME/$profile/chrome" "$FIREFOX_HOME/$profile/chrome-$(date +%F_%H%M%S_%N)"
+	fi
 
-  pushd "$TMP" || exit
-  cp -R user.js updater.sh prefsCleaner.sh "$FIREFOX_HOME/$profile"
+	mkdir -p "$FIREFOX_HOME/$profile/chrome"
+	if [ "$config" == "rec" ]; then
+		cp -R ./chrome/ui ./chrome/content ./chrome/*-rec/* "$FIREFOX_HOME/$profile/chrome"
+	else
+		cp -R ./chrome/ui ./chrome/content ./chrome/*-coding/* "$FIREFOX_HOME/$profile/chrome"
+	fi
 
-  pushd "$FIREFOX_HOME/$profile" || exit
-  sh ./updater.sh -d -s -o user.js-overrides
-  echo "Profile '$profile' Completed!"
+	if [ -d "$FIREFOX_HOME/$profile/user.js-overrides" ]; then
+		mv "$FIREFOX_HOME/$profile/user.js-overrides" "$FIREFOX_HOME/$profile/user.js-overrides-$(date +%F_%H%M%S_%N)"
+	fi
+
+	mkdir -p "$FIREFOX_HOME/$profile/user.js-overrides"
+	cp -R ./user.js-overrides/_base.js ./user.js-overrides/*-"$config"/* "$FIREFOX_HOME/$profile/user.js-overrides"
+
+	pushd "$TMP" || exit
+	cp -R user.js updater.sh prefsCleaner.sh "$FIREFOX_HOME/$profile"
+
+	pushd "$FIREFOX_HOME/$profile" || exit
+	sh ./updater.sh -d -s -o user.js-overrides
+	echo "Profile '$profile' Completed!"
+}
+
+config_profile() {
+	configs=("coding" "dev" "main" "rec" "rgt")
+
+	profile="$1"
+
+	if [ -z "$profile" ]; then
+		echo "Profile not found... Exiting..."
+		exit 1
+	fi
+
+	config="$2"
+
+	for i in "${configs[@]}"; do
+		if [ "$profile" == "$i" ]; then
+			config="$i"
+			break
+		fi
+	done
+
+	if [ -z "$config" ]; then
+		echo "Config not found... Do you want to use default config (main)? [y/n]"
+		read -r ans
+		if [ "$ans" == "y" ]; then
+			config="main"
+			echo "Using default config: $config"
+		else
+			echo "Exiting..."
+			exit 1
+		fi
+	fi
+
+	for i in "${configs[@]}"; do
+		if [ "$config" == "$i" ]; then
+			echo "Using config: $config"
+			setup "$profile" "$config"
+			return
+		fi
+	done
+
+	echo "Invalid config: $config"
 }
 
 while [ "$#" -gt 0 ]; do
-  curr=$1
-  shift
+	curr=$1
+	shift
 
-  install_essentials
-  case "$curr" in
-  -install) clone_config ;;
-  -coding) config_branch "coding" ;;
-  -dev) config_branch "dev" ;;
-  -main) config_branch "main" ;;
-  -rec) config_branch "rec" ;;
-  -rgt) config_branch "rgt" ;;
-  -social) config_branch "social" ;;
-  -all)
-    config_branch "coding" &&
-      config_branch "dev" &&
-      config_branch "main" &&
-      config_branch "rec" &&
-      config_branch "rgt" &&
-      config_branch "social" &&
-      echo "All profiles completed!"
-    ;;
-  *) echo "Unavailable command... $curr" ;;
-  esac
+	install_essentials
+	case "$curr" in
+	-clone) clone_config ;;
+	-install)
+		mkdir -p "$HOME/.local/bin"
+		if [ -e "$HOME/.local/bin/fuj" ]; then
+			echo "fuj already exists in $HOME/.local/bin"
+      exit 1
+		fi
+    ln -s "$HOME/.dots/firefox-user.js/setup.sh" "$HOME/.local/bin/fuj"
+    echo "Installed 'fuj' command"
+		;;
+	-new)
+		profile=$1
+		if [ -z "$profile" ]; then
+			echo "missing profile"
+			exit 1
+		fi
+		config="$2"
+		if [ -n "$config" ]; then
+			shift
+		fi
+		config_profile "$profile" "$config"
+		;;
+	-coding) config_profile "coding" ;;
+	-dev) config_profile "dev" ;;
+	-main) config_profile "main" ;;
+	-rec) config_profile "rec" ;;
+	-rgt) config_profile "rgt" ;;
+	-social) config_profile "social" "dev" ;;
+	-all)
+		config_profile "coding" &&
+			config_profile "dev" &&
+			config_profile "main" &&
+			config_profile "rec" &&
+			config_profile "rgt" &&
+			config_profile "social" "dev" &&
+			echo "All profiles completed!"
+		;;
+	*) echo "Unavailable command... $curr" ;;
+	esac
 done
