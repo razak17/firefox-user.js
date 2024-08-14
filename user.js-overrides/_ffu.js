27c8ea3bae56c7bb7819ac566495f661db442f83
Razak Mo <moabdulrazak17@gmail.com>
2024-08-14 13:09:03 +0000

    feat: add FF_ULTIMA overrides

---
+++ user.js-overrides/_ffu.js
@@ -0,0 +1,8 @@
+/*tabs*/
+user_pref("ultima.tabs.closetabsbutton", true);
+
+/*url bar*/
+user_pref("ultima.urlbar.hidebuttons", true);
+
+/*color schemes*/
+user_pref("user.theme.dark.catppuccin-mocha", true);
