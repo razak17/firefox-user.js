87eb9fe43e358bde5cedd95cea9052d25b4375db
Razak Mo <moabdulrazak17@gmail.com>
2024-08-25 15:37:33 +0000

    feat: hide toolbar in compact mode for zen

--- user.js-overrides/_zen.js
+++ user.js-overrides/_zen.js
@@ -1,3 +1,3 @@
 user_pref("zen.theme.accent-color", "#74d7cb");
 user_pref("zen.view.sidebar-expanded", true);
-user_pref("zen.welcomeScreen.seen", true);
+user_pref("zen.view.compact.hide-toolbar", true);
