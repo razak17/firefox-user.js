d659d6e2fb5934e90964c8d59f6ab2939dcbff80
Razak Mo <moabdulrazak17@gmail.com>
2024-09-28 01:02:21 +0000

    chore: delete cookies

--- user.js-overrides/2-rec/_custom.js
+++ user.js-overrides/2-rec/_custom.js
@@ -4,6 +4,7 @@
 
 /*** [SECTION 2800]: SHUTDOWN & SANITIZING ***/
 user_pref("network.cookie.lifetimePolicy", 0);
+user_pref("extensions.webextensions.restrictedDomains", "");
 
 /*** [SECTION 4500]: RFP (RESIST FINGERPRINTING) **/
 user_pref("privacy.resistFingerprinting.letterboxing", false); // reduced screen size
@@ -12,12 +13,12 @@ user_pref("privacy.resistFingerprinting.letterboxing", false); // reduced screen
 user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
 
 /** SANITIZE ON SHUTDOWN : ALL OR NOTHING ***/
-user_pref("privacy.clearOnShutdown.cookies", false);
+// user_pref("privacy.clearOnShutdown.cookies", false);
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.clearOnShutdown.openWindows", false);
 user_pref("privacy.cpd.openWindows", false);
 user_pref("privacy.cpd.history", false);
-user_pref("privacy.cpd.cookies", false);
+// user_pref("privacy.cpd.cookies", false);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 
 /* 1601: control when to send a cross-origin referer
--- user.js-overrides/4-main/_custom.js
+++ user.js-overrides/4-main/_custom.js
@@ -9,10 +9,10 @@ user_pref("network.cookie.lifetimePolicy", 0);
 user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
 
 /** SANITIZE ON SHUTDOWN : ALL OR NOTHING ***/
-user_pref("privacy.clearOnShutdown.cookies", false);
+// user_pref("privacy.clearOnShutdown.cookies", false);
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.clearOnShutdown.openWindows", false);
 user_pref("privacy.cpd.openWindows", false);
 user_pref("privacy.cpd.history", false);
-user_pref("privacy.cpd.cookies", false);
+// user_pref("privacy.cpd.cookies", false);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
