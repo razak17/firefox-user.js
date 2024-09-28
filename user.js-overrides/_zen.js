055e16c3f3b89b4df192196718c0e1ad28373006
Razak Mo <moabdulrazak17@gmail.com>
2024-09-28 18:45:42 +0000

    chore(zen): show vertical tabs on the left side

--- user.js-overrides/_zen.js
+++ user.js-overrides/_zen.js
@@ -1,3 +1,4 @@
-user_pref("zen.theme.accent-color", "#74d7cb");
+user_pref("zen.theme.accent-color", "#1d7c78");
 user_pref("zen.view.sidebar-expanded", false);
 user_pref("zen.view.compact.hide-toolbar", true);
+user_pref("zen.tabs.vertical.right-side", false);
