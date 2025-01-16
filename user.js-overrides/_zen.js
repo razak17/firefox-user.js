4a6a36ffc407fadca3276509ace21e7098e912eb
Abdul Razak <moabdulrazak17@gmail.com>
2025-01-16 14:20:53 +0000

    chore(zen): disable tab unloader

--- user.js-overrides/_zen.js
+++ user.js-overrides/_zen.js
@@ -5,7 +5,9 @@ user_pref("zen.view.sidebar-expanded", true);
 user_pref("zen.workspaces.show-workspace-indicator", false);
 user_pref("zen.tabs.vertical.right-side", false);
 user_pref("zen.glance.activation-method", "shift");
+user_pref("zen.tab-unloader.enabled", false); // use Auto Discard addon instead
 user_pref("zen.tab-unloader.timeout-minutes", 60);
 user_pref("zen.view.show-bottom-border", true);
 user_pref("zen.tabs.show-newtab-vertical", false);
 // user_pref("zen.workspaces.icons", ["🌐", "💡", "📚", "🐧","🍿", "🐼","📺", "📂","🖥️","🌍", "📁", "📎", "💼", "📝", "📅", "📊", "🧠", "🚀", "🎯", "🔒", "💡", "🎨", "🛠️", "🧩", "💻", "📱", "🎓", "📚", "⚙️", "🎉", "🕹️", "🛒", "🔍","🧪","🔧","🗝️","🎧","🎮"]);
+// user_pref("zen.keyboard.shortcuts", "{\"zenSplitViewGrid\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"G\"},\"zenSplitViewVertical\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"V\"},\"zenSplitViewHorizontal\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"H\"},\"zenSplitViewClose\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"U\"},\"zenChangeWorkspace\":{\"ctrl\":true,\"alt\":false,\"shift\":true,\"meta\":false,\"key\":\"E\"},\"zenToggleCompactMode\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"C\"},\"zenToggleCompactModeSidebar\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"S\"},\"zenToggleCompactModeToolbar\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"T\"},\"zenToggleWebPanels\":{\"ctrl\":true,\"alt\":false,\"shift\":true,\"meta\":false,\"key\":\"P\"}}");
