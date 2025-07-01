d92d7b99d436f13f1d16f6a744f4eddbbe0166c1
Abdul Razak <moabdulrazak17@gmail.com>
2025-07-01 03:14:35 +0000

    chore(zen): remove duplicate pref

--- user.js-overrides/_zen.js
+++ user.js-overrides/_zen.js
@@ -9,13 +9,12 @@ user_pref("zen.theme.accent-color", "#1d7c78");
 // user_pref("zen.view.sidebar-expanded", false);
 user_pref("zen.view.compact.hide-toolbar", false);
 user_pref("zen.view.sidebar-expanded", true);
-user_pref("zen.tabs.vertical.right-side", false);
+user_pref("zen.tabs.vertical.right-side", true);
 user_pref("zen.glance.activation-method", "shift");
 user_pref("zen.tab-unloader.enabled", false); // use Auto Discard addon instead
 user_pref("zen.tab-unloader.timeout-minutes", 60);
 user_pref("zen.view.show-bottom-border", true);
 user_pref("zen.tabs.show-newtab-vertical", false);
-user_pref("zen.tabs.vertical.right-side", true);
 user_pref("zen.view.experimental-no-window-controls", true); // remove animation when I hover over top edge of window
 // user_pref("zen.workspaces.icons", ["🌐", "💡", "📚", "🐧","🍿", "🐼","📺", "📂","🖥️","🌍", "📁", "📎", "💼", "📝", "📅", "📊", "🧠", "🚀", "🎯", "🔒", "💡", "🎨", "🛠️", "🧩", "💻", "📱", "🎓", "📚", "⚙️", "🎉", "🕹️", "🛒", "🔍","🧪","🔧","🗝️","🎧","🎮"]);
 user_pref("zen.keyboard.shortcuts", "{\"zenSplitViewGrid\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"G\"},\"zenSplitViewVertical\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"V\"},\"zenSplitViewHorizontal\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"H\"},\"zenSplitViewClose\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"U\"},\"zenChangeWorkspace\":{\"ctrl\":true,\"alt\":false,\"shift\":true,\"meta\":false,\"key\":\"E\"},\"zenToggleCompactMode\":{\"ctrl\":false,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\";\"},\"zenToggleCompactModeSidebar\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"S\"},\"zenToggleCompactModeToolbar\":{\"ctrl\":true,\"alt\":true,\"shift\":false,\"meta\":false,\"key\":\"T\"},\"zenToggleWebPanels\":{\"ctrl\":true,\"alt\":false,\"shift\":true,\"meta\":false,\"key\":\"P\"}}");
